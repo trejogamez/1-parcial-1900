97c643eae483cb810a93c4eb76d38431bb683267
trejogamez <stephanytrejoblue@gmail.com>
2023-02-04 01:05:17 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INGRESE EL NUMERO:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Location = new System.Drawing.Point(180, 35);
+            this.txtbox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(116, 20);
+            this.txtbox1.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(345, 33);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(88, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "aceptar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "NUMEROS IMPARES Y PARES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ ejercicio 1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            decimal num1 = 0;
+            num1 = Convert.ToDecimal(txtbox1.Text);
+            if (num1 % 2 == 0)
+            {
+                MessageBox.Show("EL NUMERO ES PAR:");
+            }
+            else { MessageBox.Show("EL NUMERO ES IMPAR:");}
+
+
+            if (num1 > 0) 
+            {
+                MessageBox.Show("EL NUMERO ES POSITIVO");
+            }
+            else { MessageBox.Show("EL NUMERO ES NEGATIVO"); }
+
+
+        }
+    }
+}
